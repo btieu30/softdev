afdef496a19743f12bd90e4215157e3fb38b75cf
Brianna <btieu30@stuy.edu>
2022-09-21 13:16:37 -0400

    string_match

---
+++ 03_py/warmup-2/string_match.py
@@ -0,0 +1,22 @@
+def string_match(a, b):
+    count = 0
+
+    #find the string with the shorter length
+    if len(a) < len(b):
+        shorter = a
+        longer = b
+    else:
+        shorter = b
+        longer = a
+    
+    #loop through the shorter string, check every two chars to check for matches
+    for x in range(len(shorter) - 1):
+        if shorter[x] == longer[x] and shorter[x+1] == longer[x+1]:
+            count = count + 1
+
+    #return the overall count
+    return count
+
+    return string_match('xxcaazz', 'xxbaaz')
+    return string_match('abc', 'abc')
+    return string_match('abc', 'axc')
