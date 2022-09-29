953fb05e9375898d475b7445100e33cb4a1e91f3
Brianna <btieu30@stuy.edu>
2022-09-29 13:15:16 -0400

    nevermind

--- 05_bitstream/krewes.py
+++ 05_bitstream/krewes.py
@@ -1,5 +1,5 @@
 '''
-Git Bri in May: Gitae Park, Brianna Tieu, May Qiu
+Git Bri in May: Brianna Tieu, May Qiu
 SoftDev
 K04 -- krewes/Making a random generator to return the name of a student from any class using dictionaries
 2022-09-23
@@ -11,8 +11,8 @@ OP Summary for randomdevo()
 4) Choose a random devo in the period
 5) Print the selected devo's name and period
 OP Summary for morethanone()
-1) Create variable numberofdevos to take user input
-2) Check if numberofdevos is within range (1 < numberofdevos < 106)
+1) Create variable number_of_devos to take user input
+2) Check if number_of_devos is within range (1 < number_of_devos < 106)
    If so, proceed.
    Else, user selects new numbers until there is one in range.
 3) Create for loop that continues until the required number of devos are printed.
@@ -33,6 +33,7 @@ DISCO
 We needed to cast the .keys into a list otherwise we got the Type Error: "dict_keys" object is not subscriptable
 '''
 import random
+'''
 krewes = {
            2:["NICHOLAS",  "ANTHONY",  "BRIAN",  "SAMUEL",  "JULIA",  "YUSHA",  "CORINA",  "CRAIG",  "FANG MIN",  "JEFF",  "KONSTANTIN",  "AARON",  "VIVIAN",  "AYMAN",  "TALIA",  "FAIZA",  "ZIYING",  "YUK KWAN",  "DANIEL",  "WEICHEN",  "MAYA",  "ELIZABETH",  "ANDREW",  "VANSH",  "JONATHAN",  "ABID",  "WILLIAM",  "HUI",  "ANSON",  "KEVIN",  "DANIEL",  "IVAN",  "JASMINE",  "JEFFREY", "Ruiwen"],
            7:["DIANA",  "DAVID",  "SAM",  "PRATTAY",  "ANNA",  "JING YI",  "ADEN",  "EMERSON",  "RUSSELL",  "JACOB",  "WILLIAM",  "NADA",  "SAMANTHA",  "IAN",  "MARC",  "ANJINI",  "JEREMY",  "LAUREN",  "KEVIN",  "RAVINDRA",  "SADI",  "EMILY",  "GITAE",  "MAY",  "MAHIR",  "VIVIAN",  "GABRIEL",  "BRIANNA",  "JUN HONG",  "JOSEPH",  "MATTHEW",  "JAMES",  "THOMAS",  "NICOLE",  "Karen"],
@@ -47,11 +48,11 @@ def randomdevo():
 
 
 def morethanone():
-    numberofdevos = int(input("How many devos would you like to summon? Pick a number between 1-106.\n"))
-    while numberofdevos > 106 or numberofdevos < 1:
+    number_of_devos = int(input("How many devos would you like to summon? Pick a number between 1-106.\n"))
+    while number_of_devos > 106 or number_of_devos < 1:
         print("You are dumb. Read directions.")
-        numberofdevos = int(input("How many people would you like? Pick carefully\n"))
-    for x in range(numberofdevos):
+        number_of_devos = int(input("How many people would you like? Pick carefully\n"))
+    for x in range(number_of_devos):
         period = list(krewes.keys())
         randp = random.choice(period)
         devos = list(krewes.get(randp))
@@ -60,9 +61,29 @@ def morethanone():
         krewes[randp].remove(randd)
         if len(devos) == 1:
             krewes.pop(randp)
+'''
 
+krewes_file = open("krewes.txt", "r")
+text = krewes_file.read()
 
+second = {}
+seventh = {}
+eighth = {}
+
+data = text.split("@@@")
+for x in data:
+    period = x.split("$$$")[0]
+    name = x.split("$$$")[1]
+    ducky = x.split("$$$")[2]
+    if period == "2":
+        second.add(name, ducky)
+    if period == "7":
+        seventh.add(name, ducky)
+    if period == "8":
+        eighth.add(name, ducky)
+
+print(seventh)
 
 
 #randomdevo()
-morethanone()
+#morethanone()
